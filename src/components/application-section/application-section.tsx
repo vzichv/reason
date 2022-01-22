bfeed5cf67e25e8ac26aefe1d741a729f2c598dc
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-21 20:19:51 -0700

    Init "application-section" component

---
+++ src/components/application-section/application-section.tsx
@@ -0,0 +1,11 @@
+import styles from './application-section.module.sass';
+
+function ApplicationSection(): JSX.Element {
+  return(
+    <section>
+
+    </section>
+  );
+}
+
+export default ApplicationSection;
