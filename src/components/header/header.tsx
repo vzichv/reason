13ff04cf793da8548b3003cf9349bcd446b70d0b
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-21 19:56:53 -0700

    Create header structure and styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,21 @@
-import './header.sass';
+import styles from './header.module.sass';
 
 function Header(): JSX.Element {
   return(
-    <>
-    </>
+    <header className={styles.body}>
+
+      <div className={`${styles.company} ${styles.left}`}>
+        <h1 className={styles.name}>РЕЗОН</h1>
+        <span className={styles.type}>АВТОШКОЛА</span>  
+      </div>
+
+      <div className={`${styles.company} ${styles.right}`}>
+        <h1 className={styles.telephone}>+7 (999) 999-99-99</h1> {/* добавить подчёркивание с помощью border */}
+        <span className={styles.workingHours}>На связи с 10:00 до 20:00</span>
+        <address className={styles.address}>пр. Троицкий, дом 63, этаж 4, каб. 47</address>
+      </div>
+      
+    </header>
   );
 }
 
