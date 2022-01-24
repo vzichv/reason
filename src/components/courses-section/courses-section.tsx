9f1f22336ca4260f11f4ab5f563b2165fd6e46ed
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-24 12:03:21 -0700

    Create "courses-section" component

--- src/components/courses-section/courses-section.tsx
+++ src/components/courses-section/courses-section.tsx
@@ -1,8 +1,58 @@
 import styles from './courses-section.module.sass';
+import SectionHeader from './../section-header/section-header';
+import SubsectionHeader from './../subsection-header/subsection-header';
 
 function CoursesSection(): JSX.Element {
   return(
-    <section>
+    <section className={styles.body}>
+      <SectionHeader backgroundColor={2} text='Обучение в автошколе!' />
+      <SubsectionHeader backgroundColor={2} text='Курсы'/>
+      <div className={styles.coursesBlock}>
+        <ul className={styles.coursesList}>
+
+          <li className={styles.course}>
+            <span className={styles.courseName}>
+              <span>•</span> Механическая коробка переключения передач
+              <abbr title="Механическая Коробка Переключения Передач"> (МКПП)</abbr>
+            </span>
+
+            <li className={styles.factAboutCourse}><span className={styles.fact}>• Категория «B»</span></li>
+          </li>
+
+          <li className={styles.course}>
+            <span className={styles.courseName}>
+              <span>•</span> Автоматическая коробка переключения передач 
+              <abbr title="Автоматическая Коробка Переключения Передач"> (АКПП)</abbr>
+            </span>
+
+            <li className={styles.factAboutCourse}><span className={styles.fact}>• Категория «B»</span></li>
+          </li>
+
+          <li className={styles.course}>
+            <span className={styles.courseName}><span>•</span> Восстановление навыков</span>
+
+            <li className={styles.factAboutCourse}><span className={styles.fact}>• Активное маневрирование</span></li>
+            <li className={styles.factAboutCourse}><span className={styles.fact}>• Возможен выезд на Вашем авто!</span></li>
+          </li>
+
+          <li className={styles.course}>
+            <span className={styles.courseName}><span>•</span> Паркинг</span>
+            <li className={styles.factAboutCourse}>
+              <span className={styles.fact}>• Изучение 8 видов парковки в реальных условиях:</span>
+
+              <li><span>• Парковка передом под прямым углом на обе стороны</span></li>
+              <li><span>• Параллельная парковка на правую</span></li>
+              <li><span>• Парковка на левую сторону дороги</span></li>
+              <li><span>• Заезд в бокс задним ходом</span></li>
+              <li><span>• Заезд в бокс задним ходом под углом 90 ° </span></li>
+              <li><span>• Заезд в бокс задним ходом в ограниченном пространстве</span></li>
+              <li><span>• Разворот в ограниченном пространстве</span></li>
+              <li><span>• Движение в крытых паркингах</span></li>
+            </li>
+          </li>
+
+        </ul>
+      </div>
     </section>
   );
 }
--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,11 +1,13 @@
 import Header from './../../components/header/header';
-import ApplicationSection from '../../components/application-section/application-section';
+import ApplicationSection from './../../components/application-section/application-section';
+import CoursesSection from './../../components/courses-section/courses-section';
 
 function Landing(): JSX.Element {
   return(
     <>
       <Header />
       <ApplicationSection />
+      <CoursesSection />
     </>
   );
 }
