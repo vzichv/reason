bcb808198ac9c80a20fb270f9abd5a5268d9bb8a
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-24 12:06:13 -0700

    Init "reviews-section" component

---
+++ src/components/reviews-section/reviews-section.tsx
@@ -0,0 +1,13 @@
+import styles from './reviews-section.module.sass';
+import SectionHeader from './../section-header/section-header';
+import SubsectionHeader from './../subsection-header/subsection-header';
+
+function ReviewsSection(): JSX.Element {
+  return(
+    <section>
+      
+    </section>
+  );
+}
+
+export default ReviewsSection;
