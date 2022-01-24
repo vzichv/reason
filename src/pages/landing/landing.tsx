0967e528f742249d0bfcf6fcc502713ad350b1f4
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-24 15:48:11 -0700

    Add profile image and edit background image

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,6 +1,7 @@
 import Header from './../../components/header/header';
 import ApplicationSection from './../../components/application-section/application-section';
 import CoursesSection from './../../components/courses-section/courses-section';
+import ReviewsSection from './../../components/reviews-section/reviews-section';
 
 function Landing(): JSX.Element {
   return(
@@ -8,6 +9,7 @@ function Landing(): JSX.Element {
       <Header />
       <ApplicationSection />
       <CoursesSection />
+      <ReviewsSection />
     </>
   );
 }
