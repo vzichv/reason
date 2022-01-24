6668cdb92850a47243ec42f88bb7744c9bd167d0
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-23 19:21:06 -0700

    Create "subsection-header" component

--- src/components/subsection-header/subsection-header.tsx
+++ src/components/subsection-header/subsection-header.tsx
@@ -1,8 +1,26 @@
 import styles from './subsection-header.module.sass';
 
-function SubsectionHeader(): JSX.Element {
+interface Header {
+  text: string;
+  backgroundColor: number; // 1, 2, 3
+}
+
+function SubsectionHeader(props: Header): JSX.Element {
+  const { text, backgroundColor } = props;
+
+  function setBackgroundColor(): string {
+    return backgroundColor === 1 ? styles.a : backgroundColor === 2 ? styles.b : styles.c;
+  }
+
+  function setTextColor(): string {
+    return backgroundColor === 1 ? styles.ta : backgroundColor === 2 ? styles.tb : styles.tc;
+  }
+
   return(
-    <div>
+    <div className={styles.body}>
+      <div className={styles.formForPrinting}></div>
+      <div className={`${styles.distinctiveStripe} ${setBackgroundColor()}`}></div>
+      <h3 className={`${styles.headerText} ${setTextColor()}`}>{text}</h3>
     </div>
   );
 }
