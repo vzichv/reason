005d92b46ec5285bce869348bb65be087a171ff6
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-24 12:07:48 -0700

    Remove extra dependency

--- src/components/reviews-section/reviews-section.tsx
+++ src/components/reviews-section/reviews-section.tsx
@@ -1,6 +1,5 @@
 import styles from './reviews-section.module.sass';
 import SectionHeader from './../section-header/section-header';
-import SubsectionHeader from './../subsection-header/subsection-header';
 
 function ReviewsSection(): JSX.Element {
   return(
