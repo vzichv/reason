24786735be37f2c27cf4204a3cad8f70509bb530
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-24 17:05:35 -0700

    Create "offer-component"

--- src/components/offer-component/offer-component.tsx
+++ src/components/offer-component/offer-component.tsx
@@ -1,10 +1,27 @@
 import styles from './offer-component.module.sass';
 
-function OfferSection(): JSX.Element {
+interface Offer {
+  name: string;
+  description: string;
+  button: string;
+}
+
+function OfferComponent(props: Offer): JSX.Element {
+  const { name, description, button } = props;
+
   return(
-    <section>
+    <section className={styles.offer}>
+      
+      <header className={styles.header}>
+        <h1>{name}</h1>
+      </header>
+      <div className={styles.body}>
+        <span>{description}</span>
+        <input className={styles.button} type='submit' value={button} />
+      </div>
+
     </section>
   );
 }
 
-export default OfferSection;
+export default OfferComponent;
