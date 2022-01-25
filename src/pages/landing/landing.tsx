fbeec74c9d7b8dc051c22f96ec4504208cf6ba25
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-24 17:09:46 -0700

    Add "offer-section" in landing page

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -2,6 +2,7 @@ import Header from './../../components/header/header';
 import ApplicationSection from './../../components/application-section/application-section';
 import CoursesSection from './../../components/courses-section/courses-section';
 import ReviewsSection from './../../components/reviews-section/reviews-section';
+import OfferSection from './../../components/offer-section/offer-section';
 
 function Landing(): JSX.Element {
   return(
@@ -10,6 +11,7 @@ function Landing(): JSX.Element {
       <ApplicationSection />
       <CoursesSection />
       <ReviewsSection />
+      <OfferSection />
     </>
   );
 }
