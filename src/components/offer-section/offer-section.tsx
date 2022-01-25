6d7ae8fc7248af449f7414fa4c3b3ee78f574c76
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-24 17:07:41 -0700

    Create "offer-section" component and add offers

--- src/components/offer-section/offer-section.tsx
+++ src/components/offer-section/offer-section.tsx
@@ -1,8 +1,18 @@
 import styles from './offer-section.module.sass';
 
+import SectionHeader from './../section-header/section-header';
+import OfferComponent from './../offer-component/offer-component';
+
 function OfferSection(): JSX.Element {
   return(
-    <section>
+    <section className={styles.body}>
+      <SectionHeader backgroundColor={3} text='Актуальные предложения!' />
+      <div className={styles.margin}></div>
+      <div className={styles.offers}>
+        <OfferComponent name='РАССРОЧКА' description='Обучение при первом взносе размером 5000 ₽' button='ЗАПИСАТЬСЯ' />
+        <OfferComponent name='ОНЛАЙН ТЕОРИЯ' description='Мы поможем Вам изучить всю нужную теорию не выходя из дома!' button='ЗАПИСАТЬСЯ' />
+        <OfferComponent name='СКИДКА' description='Вступи в нашу группу ВК и получишь скидку!' button='ВСТУПИТЬ' />
+      </div>
     </section>
   );
 }
