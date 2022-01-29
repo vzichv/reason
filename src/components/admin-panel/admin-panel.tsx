af629a89f1577859b52f4fe7d17b30413f59ccd9
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-29 15:41:21 -0700

    Create "admin-panel" component

---
+++ src/components/admin-panel/admin-panel.tsx
@@ -0,0 +1,38 @@
+import styles from './admin-panel.module.sass';
+
+import EditCourses from './edit-courses/edit-courses';
+import EditReviews from './edit-reviews/edit-reviews';
+import EditOffers from './edit-offers/edit-offers';
+import EditApplications from './edit-applications/edit-applications';
+
+import { NavLink, Routes, Route } from 'react-router-dom';
+
+function AdminComponent(): JSX.Element {
+  return (
+    <div className={styles.background}>
+
+      <section className={styles.mainBlock}>
+
+        <nav className={styles.navMenu}>
+          <NavLink to="courses" className={styles.button}>Курсы</NavLink>
+          <NavLink to="reviews" className={styles.button}>Отзывы</NavLink>
+          <NavLink to="offers"  className={styles.button}>Предложения</NavLink>
+          <NavLink to="applications"  className={styles.button}>Заявки</NavLink>
+        </nav>
+
+        <div className={styles.body}>
+          <Routes>
+            <Route path="courses" element={ <EditCourses /> } />
+            <Route path="reviews" element={ <EditReviews /> } />
+            <Route path="offers"  element={ <EditOffers />  } />
+            <Route path="applications"  element={ <EditApplications />  } />
+          </Routes>
+        </div>
+
+      </section>
+
+    </div>
+  )
+}
+
+export default AdminComponent;
