9e97a23ac5b1bab408302a6e5638fd25e9ae8817
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-29 15:42:51 -0700

    Create "admin" page

---
+++ src/pages/admin/admin.tsx
@@ -0,0 +1,11 @@
+import AdminPanel from './../../components/admin-panel/admin-panel';
+
+function Admin(): JSX.Element {
+  return (
+    <>
+      <AdminPanel />
+    </>
+  )
+}
+
+export default Admin;
