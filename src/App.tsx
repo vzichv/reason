f910704d127c662415550ab5c62e9d03f656aa92
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-29 15:43:45 -0700

    Create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Landing from './pages/landing/landing';
+import Admin from './pages/admin/admin'
 
 function App(): JSX.Element {
   return (
-    <>
-      <Landing />
-    </>
+    <Routes>
+      <Route path='/' element={ <Landing /> } />
+      <Route path='/admin/*' element={ <Admin /> } />
+    </Routes>
   );
 }
 
